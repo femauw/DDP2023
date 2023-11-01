a7b68282fc8245a4afe1ad118b2ed24b55d859e4
faqih murobbie <146088034+femauw@users.noreply.github.com>
2023-11-01 10:53:24 +0700

    Add files via upload

---
+++ Pekan6/Tugas 2.py
@@ -0,0 +1,6 @@
+# Buat lah output dari menggunakan bahasa pemprograman python dengan : 
+# 1 + 3 + 5 + 7 +9 +11 +13 + 15 +17 +19 = ….
+i = 0
+for penjumlahan in range(1,20,2):
+    i+=penjumlahan
+print("1+3+5+7+9+11+13+15+17+19 adalah", i, end=" ")
---
+++ Pekan6/pekan 6.py
@@ -0,0 +1,25 @@
+# WHILE
+# a = 0
+# while a<=10:
+    # print(a)
+    # a+=2
+
+# ganjil dari satu sampai 100
+# a = 1
+# while a<=100:
+    # print(a)
+    # a+=2
+
+# perulangan while bertingkat
+# f = 1
+# while f<=10:
+    # k = 1
+    # while k<=10:
+        # print(f*k, end=" ")
+        # k+=1
+    # print()
+    # f+=1
+
+# FOR
+# for i in range(50,0,-5):
+    # print(i, end=" ")
